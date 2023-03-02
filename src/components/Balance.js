c0f097b25ad33a0289628042d182f47b9d30f93a
Abhishek Masali <84769906+Abhimasali23@users.noreply.github.com>
2023-03-03 00:40:57 +0530

    Update Balance.js

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,54 +1,19 @@
-import React, {useState, useContext} from 'react';
+import React, { useContext } from 'react';
 import { GlobalContext } from "../context/GlobalState";
 
 
-const AddTransaction = () => {
+export const Balance = () => {
 
-    const  [text, setText] = useState('');
-    const [amount, setAmount] = useState(0);
+  const { transactions } = useContext(GlobalContext);
 
-    const { addTransaction } = useContext(GlobalContext);
+  const amounts = transactions.map((transaction) => transaction.amount);
 
-    const onSubmit = e => {
-      e.preventDefault();
-
-      const newTransaction = {
-        id: Math.floor(Math.random() * 100000000),
-        text,
-        amount: +amount
-      }
-      addTransaction(newTransaction);
-    }
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
   return (
     <>
-      <h3>Add new transaction</h3>
-      <form onSubmit={onSubmit}>
-        <div className="form-control">
-          <label htmlFor="text">Text</label>
-          <input
-            type="text"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            placeholder="Enter text..."
-          />
-        </div>
-        <div className="form-control">
-          <label htmlFor="amount">
-          Amount <br />
-          (Negative - Expense, Positive - Income)
-          </label>
-          <input
-            type="number"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-            placeholder="Enter amount..."
-          />
-        </div>
-        <button className="btn">Add Transaction</button>
-      </form>
+      <h4>Your Current Balance</h4>
+      <h1>₹{total}</h1>
     </>
   );
 }
-
-export default AddTransaction;
